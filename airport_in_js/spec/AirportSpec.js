f057d954873c2da00f478c5c95a6d6fba35fd966
Edward Andress <eddie_andress@hotmail.com>
2021-02-26 18:55:10 +0000

    Update AirportSpec.js

--- airport_in_js/spec/AirportSpec.js
+++ airport_in_js/spec/AirportSpec.js
@@ -2,7 +2,7 @@ describe('Airport', () => {
 
   // INSTRUCTIONS
   // 1. Uncomment the first test.
-  // 2. Make it pass. Don't edit any of the test code. Ever! Only takeaway.rb
+  // 2. Make it pass. Don't edit any of the test code. Ever! Only Airport.js
   // 3. When it passes, do a git commit.
   // 4. Uncomment the next test, make it pass, commit, etc.
   // 
